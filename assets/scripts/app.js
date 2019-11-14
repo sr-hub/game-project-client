ab811da3792610850e691e5032e8c6e90c603686
sr-hub <spencer.r1@outlook.com>
2019-11-14 12:50:16 -0500

    added sample code to use as model

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict'
 
+const store = require('./store')
 // use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
 
+const authEvents = require('./auth/events')
+const exampleEvents = require('./examples/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+//document.ready
 $(() => {
-  // your JS code goes here
+  authEvents.addHandlers()
+  exampleEvents.addHandlers()
 })
