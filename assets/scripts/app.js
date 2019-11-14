d2d8567ef643ec66cb75e613573c71c8e8f81b23
sr-hub <spencer.r1@outlook.com>
2019-11-14 13:08:20 -0500

    updated app to reference events. added class to divs. working events to insert letter on click.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,12 @@
 const store = require('./store')
 // use require with a reference to bundle the file and use it in this file
 
-const authEvents = require('./auth/events')
+const authEvents = require('./events')
 const exampleEvents = require('./examples/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-//document.ready
+// document.ready
 $(() => {
   authEvents.addHandlers()
   exampleEvents.addHandlers()
--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,57 +5,64 @@ const api = require('./api')
 const formFields = require('../../../lib/get-form-fields')
 const ui = require('./ui')
 
-const onChangePass = event => {
+// const onChangePass = event => {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const formData = formFields(form)
+//
+//   api.changePass(formData)
+//     .then(ui.onChangePassSuccess)
+//     .catch(ui.onChangePassFailure)
+// }
+//
+// const onSignUp = event => {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const formData = formFields(form)
+//
+//   api.signUp(formData)
+//     .then(ui.onSignUpSuccess)
+//     .catch(ui.onSignUpFailure)
+// }
+//
+// const onSignIn = event => {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const formData = formFields(form)
+//
+//   api.signIn(formData)
+//     .then(ui.onSignInSuccess)
+//     .catch(ui.onSignInFailure)
+// }
+//
+// const onSignOut = () => {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const formData = formFields(form)
+//
+//   api.signOut(formData)
+//     .then(ui.onSignOutSuccess)
+//     .catch(ui.onSignOutFailure)
+// }
+//
+// const addHandlers = event => {
+//   $('#sign-up').on('submit', onSignUp)
+//   $('#sign-in').on('submit', onSignIn)
+//   $('#change-pass').on('submit', onChangePass)
+//   $('#sign-out').on('submit', onSignOut)
+// }
+const insertMark = event => {
   event.preventDefault()
-
-  const form = event.target
-  const formData = formFields(form)
-
-  api.changePass(formData)
-    .then(ui.onChangePassSuccess)
-    .catch(ui.onChangePassFailure)
+  $(event.target).html(``)
 }
 
-const onSignUp = event => {
-  event.preventDefault()
-
-  const form = event.target
-  const formData = formFields(form)
-
-  api.signUp(formData)
-    .then(ui.onSignUpSuccess)
-    .catch(ui.onSignUpFailure)
+const addHandler = event => {
+  $('.TTT').on('click', insertMark)
 }
-
-const onSignIn = event => {
-  event.preventDefault()
-
-  const form = event.target
-  const formData = formFields(form)
-
-  api.signIn(formData)
-    .then(ui.onSignInSuccess)
-    .catch(ui.onSignInFailure)
-}
-
-const onSignOut = () => {
-  event.preventDefault()
-
-  const form = event.target
-  const formData = formFields(form)
-
-  api.signOut(formData)
-    .then(ui.onSignOutSuccess)
-    .catch(ui.onSignOutFailure)
-}
-
-const addHandlers = event => {
-  $('#sign-up').on('submit', onSignUp)
-  $('#sign-in').on('submit', onSignIn)
-  $('#change-pass').on('submit', onChangePass)
-  $('#sign-out').on('submit', onSignOut)
-}
-
 module.exports = {
   addHandlers
 }
